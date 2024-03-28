67710009ad97fd30aa166faab291c15109153b55
Astrid Gealer <astrid@gealer.email>
2024-03-28 20:32:09 +0000

    add optional for window

--- site/components/hooks/useDarkMode.ts
+++ site/components/hooks/useDarkMode.ts
@@ -1,12 +1,12 @@
 import { useSyncExternalStore } from "react";
 
 export default function useDarkMode() {
-    const matcher = window.matchMedia("(prefers-color-scheme: dark)");
+    const matcher = globalThis.window && window.matchMedia("(prefers-color-scheme: dark)");
     return useSyncExternalStore(
         watcher => {
-            matcher.addEventListener("change", watcher);
-            return () => matcher.removeEventListener("change", watcher);
+            matcher?.addEventListener("change", watcher);
+            return () => matcher?.removeEventListener("change", watcher);
         },
-        () => matcher.matches,
+        () => matcher?.matches || false,
     );
 }
