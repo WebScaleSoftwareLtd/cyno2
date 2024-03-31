19f603526ba99097de5605768194c287ad89b515
Astrid Gealer <astrid@gealer.email>
2024-03-31 13:48:51 +0100

    remove dark mode hook

--- site/components/atoms/Loading.tsx
+++ site/components/atoms/Loading.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import useDarkMode from "../hooks/useDarkMode";
 import { MoonLoader } from "react-spinners";
 
 export default function Loading() {
-   const darkMode = useDarkMode();
    return <MoonLoader
       size={50}
-      color={darkMode ? "#fff" : "#000"}
+      className="spinner-dark-handler"
    />;
 }
