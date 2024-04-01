7e592e29112fb12ea3e51bd499a64c9640fc7231
Astrid Gealer <astrid@gealer.email>
2024-04-01 23:08:34 +0100

    prettier

--- scripts/bumpNode.js
+++ scripts/bumpNode.js
@@ -10,19 +10,22 @@ async function main() {
     const root = join(__dirname, "..");
     await writeFile(join(root, ".nvmrc"), version + "\n");
 
-    const dockerfiles = [
-        "Dockerfile.bot", "Dockerfile.site",
-    ];
+    const dockerfiles = ["Dockerfile.bot", "Dockerfile.site"];
     for (const dockerfile of dockerfiles) {
         const fp = join(root, dockerfile);
-        const fileContents = (await readFile(fp, { encoding: "utf-8" })).toString();
+        const fileContents = (
+            await readFile(fp, { encoding: "utf-8" })
+        ).toString();
         const [_, rest] = fileContents.split("\n", 2);
-        await writeFile(fp, `FROM node:${version.substring(1)}-alpine
-${rest}`);
+        await writeFile(
+            fp,
+            `FROM node:${version.substring(1)}-alpine
+${rest}`,
+        );
     }
 }
 
-main().catch(err => {
+main().catch((err) => {
     console.error(err);
     process.exit(1);
 });
