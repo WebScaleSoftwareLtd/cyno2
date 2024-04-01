f7b675d81d447d3fd730cb345defe72e143b1de6
Astrid Gealer <astrid@gealer.email>
2024-04-01 17:14:18 +0100

    change the home splash height

--- site/components/atoms/HomeSplash.tsx
+++ site/components/atoms/HomeSplash.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export default function HomeSplash({ children }: React.PropsWithChildren<{}>) {
     return (
         <header
-            className="flex flex-col items-center justify-center h-[40vh] bg-gradient-to-r from-blue-600 to-cyan-600
-            dark:from-blue-900 dark:to-cyan-900 max-h-64"
+            className="flex flex-col items-center justify-center h-64 bg-gradient-to-r from-blue-600 to-cyan-600
+            dark:from-blue-900 dark:to-cyan-900"
         >
             {children}
         </header>
