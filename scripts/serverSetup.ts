dc9b946ae981e3e79859c72879c274072ab22212
Astrid Gealer <astrid@gealer.email>
2024-04-03 01:27:39 +0100

    serverSetup so far

--- scripts/serverSetup.ts
+++ scripts/serverSetup.ts
@@ -1,18 +1,190 @@
 import { $ } from "bun";
+import readline from "readline";
+
+// Build the readline interface.
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// Defines the async function to ask a question.
+function rlQuestion(question: string) {
+    return new Promise<string>((resolve) => {
+        rl.question(question, resolve);
+    });
+}
 
 // Check we have docker compose.
 try {
     await $`docker compose --version`.throws(true).quiet();
 } catch {
-    console.error("Please install Docker Compose.");
+    console.error(
+        "Please install Docker Compose. Is your version of Docker very outdated?",
+    );
     process.exit(1);
 }
 
+// Log the logo.
+console.log(`---------------------
+| Cyno Server Setup |
+---------------------
+`);
+
+// Figure out if this is a upgrade.
+const upgrade = await Bun.file("docker-compose.generated.yml").exists();
+
+// Handle the indentation.
+function indent(content: string, space: string) {
+    const s = content.split("\n");
+    for (let i = 0; i < s.length; i++) {
+        if (s[i].trim() === "") continue;
+        s[i] = space + s[i];
+    }
+    return s.join("\n");
+}
+
+// Ask which option the user wants.
+async function ask(question: string, options: string[]) {
+    let value = question + "\n";
+    for (let i = 0; i < options.length; i++) {
+        value += `\n${i + 1}) ${options[i]}${i === 0 ? " (default)" : ""}`;
+    }
+    console.log(value + "\n");
+    for (;;) {
+        const answer = (
+            await rlQuestion("Enter the number of the option you want: ")
+        ).trim();
+        if (answer === "") return options[0];
+        if (isNaN(+answer)) continue;
+        const a = +answer;
+        if (a >= 1 && a <= options.length) return options[a - 1];
+    }
+}
+
+// Build the Docker Compose configuration.
+async function generateDockerCompose(
+    turso: boolean,
+    webServer: { type: "caddy"; ssl: boolean },
+) {
+    // Get the base template.
+    let config = await Bun.file("scripts/compose/base.template.yml").text();
+
+    // Handle the libsql configuration.
+    if (turso) {
+        config +=
+            "\n" +
+            indent(
+                await Bun.file("scripts/compose/turso.template.yml").text(),
+                "    ",
+            );
+    } else {
+        config +=
+            "\n" +
+            indent(
+                await Bun.file(
+                    "scripts/compose/libsql-server.template.yml",
+                ).text(),
+                "    ",
+            );
+    }
+
+    // Handle the caddy configuration.
+    if (webServer.type === "caddy") {
+        config +=
+            "\n" +
+            indent(
+                await Bun.file(
+                    `scripts/web/caddy-${webServer.ssl ? "https" : "http"}.template.yml`,
+                ).text(),
+                "    ",
+            );
+    } else {
+        config +=
+            "\n" +
+            indent(
+                await Bun.file("scripts/web/cloudflare.template.yml").text(),
+                "    ",
+            );
+    }
+
+    // Write the configuration.
+    const w = Bun.file("docker-compose.generated.yml").writer();
+    w.write(config);
+    await w.end();
+}
+
+// Handle blank lines.
+async function noBlankResponse(question: string) {
+    for (;;) {
+        const response = (await rlQuestion(question)).trim();
+        if (response !== "") return response;
+    }
+}
+
+if (upgrade) {
+    // Log that we are upgrading!
+    console.log(`Config detected - upgrading Cyno! 🚀
+`);
+} else {
+    // Log that we are setting up the server.
+    console.log(`No config detected - setting up Cyno! 🚀
+`);
+
+    // Defines the environment variables.
+    const env: string[] = [];
+
+    // Figure out how the user wants to deploy the database.
+    const turso =
+        (await ask("How do you want to deploy the database?", [
+            "Use a locally deployed LibSQL server",
+            "Use Turso to manage the database",
+        ])) === "Use Turso to manage the database";
+    if (turso) {
+        // Get the URL.
+        const url = await noBlankResponse(
+            "Enter the connection URI for the LibSQL server (such as Turso): ",
+        );
+        env.push(`DATABASE_URL=${url}`);
+    } else {
+        // Use the local DB.
+        env.push("DATABASE_URL=libsql://db:8000");
+    }
+
+    // Ask the user how they want to deploy the web server.
+    const tls =
+        (await ask("How do you want to deploy the web server?", [
+            "Use Caddy to manage HTTPS and use ports 443/80",
+            "Use Caddy to manage HTTP and expose on port 8080",
+        ])) === "Use Caddy to manage HTTPS"
+            ? { type: "caddy" as const, ssl: true }
+            : { type: "caddy" as const, ssl: false };
+    const hostname = await noBlankResponse(
+        "Enter the hostname for the web server (for example: example.com): ",
+    );
+    env.push(
+        `SITE_ADDRESS=${hostname}`,
+        `DISCORD_REDIRECT_URI=https://${hostname}/api/auth/callback`,
+    );
+
+    // TODO: more code
+
+    // Generate the Docker Compose configuration and write the .env file.
+    await generateDockerCompose(turso, tls);
+    const w = Bun.file(".env").writer();
+    w.write(env.join("\n") + "\n");
+    await w.end();
+
+    // Log that the configuration has been generated.
+    console.log(`
+Configuration generated! 🎉
+`);
+}
+
 // Run the command.
-async function runCommand(command: string) {
+async function runCommand(command: string, allowThrow?: boolean) {
     console.log(`📜 ${command}`);
     const r = await $`${{ raw: command }}`;
-    if (r.exitCode !== 0) process.exit(r.exitCode);
+    if (r.exitCode !== 0 && !allowThrow) process.exit(r.exitCode);
 }
 
 // Build the site and copy public.
@@ -25,9 +197,14 @@ await runCommand(
     "docker run --rm -v ./server_cfg:/server_cfg cyno-site cp -r /home/node/app/site/.next/static /server_cfg/public/_next",
 );
 
-// Log the logo.
-console.log(`
----------------------
-| Cyno Server Setup |
----------------------
-`);
+// If it is an upgrade, stop the containers.
+if (upgrade)
+    await runCommand(
+        "docker compose down -f docker-compose.generated.yml",
+        true,
+    );
+
+// Start the containers whilst building the bot.
+await runCommand(
+    "docker compose up -d -f docker-compose.generated.yml --build",
+);
