a57108f506159beab9f5f3de717d628725c29a41
Astrid Gealer <astrid@gealer.email>
2024-04-11 08:59:38 +0100

    allow shard information on boot

--- bot/index.cjs
+++ bot/index.cjs
@@ -1,9 +1,27 @@
 "use strict";
 
+// Get the shard count.
+const shardCount = parseInt(process.env.SHARD_COUNT || "1");
+if (isNaN(shardCount)) throw new Error("Invalid shard count");
+
+// Get the shard ID.
+let shardId;
+if (process.env.SHARD_ID) {
+    shardId = parseInt(process.env.SHARD_ID);
+    if (isNaN(shardId)) throw new Error("Invalid shard ID");
+} else if (process.env.POD_NAME) {
+    shardId = parseInt(process.env.POD_NAME.split("-").pop());
+    if (isNaN(shardId)) throw new Error("Invalid shard ID");
+} else {
+    shardId = 0;
+}
+
 // Build the client.
 const { Client } = require("discord.js");
 const client = new Client({
     intents: ["Guilds", "GuildMessages", "GuildMembers"],
+    shardCount,
+    shardId,
 });
 
 // Require the bundle.
