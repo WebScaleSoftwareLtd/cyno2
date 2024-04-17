fe721b4b7feb7b7a9b465f690e1e63b8df72782c
Astrid Gealer <astrid@gealer.email>
2024-04-17 09:43:32 +0100

    Revert "turn off timely for now"
    This reverts commit 6d774db96787bb902885c7fcfe2c06f0b16a8d02.

--- bot/src/commands/timely.ts
+++ bot/src/commands/timely.ts
@@ -9,9 +9,63 @@ export const description =
     "Used to claim your free currency in a guild if enabled.";
 
 export async function run(interaction: CommandInteraction) {
-    return error(
+    // Get the timely config.
+    const gid = BigInt(interaction.guildId!);
+    const guildTimelyConfig = await client.query.guildTimelyConfig
+        .findFirst({
+            where: (configs, { eq }) => eq(configs.guildId, gid),
+        })
+        .execute();
+    if (!guildTimelyConfig?.enabled) {
+        return error(
+            interaction,
+            "Timely Collections Disabled",
+            "Timely collections are disabled in this guild.",
+        );
+    }
+
+    // Find the user's last collection.
+    const uid = BigInt(interaction.user.id);
+    const lastCollection = await client.query.timelyCollections
+        .findFirst({
+            where: (collections, { eq, and }) =>
+                and(eq(collections.guildId, gid), eq(collections.userId, uid)),
+        })
+        .execute();
+    if (lastCollection && lastCollection.lastCollected >= new Date()) {
+        const nextCollection =
+            lastCollection.lastCollected.getTime() +
+            guildTimelyConfig.hoursBetweenCollections * 60 * 60 * 1000;
+        return error(
+            interaction,
+            "Not So Fast!",
+            `You can claim your next collection <t:${nextCollection / 1000}:R>.`,
+        );
+    }
+
+    // Add to the users balance.
+    await add(gid, uid, BigInt(guildTimelyConfig.amount), "Timely collection");
+
+    // Update the collection time.
+    const now = new Date();
+    await client
+        .insert(timelyCollections)
+        .values({
+            guildId: gid,
+            userId: uid,
+            lastCollected: now,
+        })
+        .onConflictDoUpdate({
+            target: [timelyCollections.guildId, timelyCollections.userId],
+            set: { lastCollected: now },
+        })
+        .execute();
+
+    // Get the guild configuration.
+    const guild = await getGuild(gid);
+    return success(
         interaction,
-        "Timely Collections Disabled",
-        "Timely collections are disabled.",
+        "Timely Collection Complete",
+        `You have collected ${guild.currencyEmoji} ${guildTimelyConfig.amount}.`,
     );
 }
